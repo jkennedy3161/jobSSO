5e40314b4c8a20ffe51eb333537f14f73817f399
jkennedy3161 <kennedyjt88@gmail.com>
2017-11-09 16:39:01 -0800

    add logic for updating skills

--- client/app/dummyData.js
+++ client/app/dummyData.js
@@ -1,30 +1,48 @@
 const skills = [
   {
     'name': 'java',
-    'count': '3'
+    'count': '0'
   },
   {
     'name': 'html',
-    'count': '12'
+    'count': '0'
   },
   {
     'name': 'reactjs',
-    'count': '4'
+    'count': '0'
   }
 ];
 
 const $addSkill = $('#addSkill');
-const $here = $('.here');
-$('document').ready(preLoad);
+const $skill = $('.skill');
+const $count = $('.count');
+$('document').ready(preLoad, () => {
+
+});
 
 function preLoad() {
-  skills.forEach(skill => {
-    $here.prepend(`<li>${skill.name}</li>`);
+  skills.forEach((skill, i) => {
+    $skill.prepend(`<li id=${i}>${skill.name}</li>`);
+    $count.prepend(`<li id=${i}>${skill.count}</li>`);
   });
-
+  $count.children().click((e) => {
+    console.log(e);
+    let c = Number(e.target.innerHTML, 2);
+    c++;
+    skills[e.target.id].count++;
+    console.table(skills);
+    $count.find(`li#${e.target.id}`).text(skills[e.target.id].count);
+  });
 }
 
 $addSkill.click(() => {
-  skills.unshift({'name': 'css', 'count': '2'});
-  $here.prepend(`<li>${skills[0].name}</li>`);
+  let name = $('#skill').val();
+  skills.unshift({'name': name, 'count': '0'});
+  $skill.prepend(`<li>${skills[0].name}</li>`);
+  $count.prepend(`<li>${skills[0].count}</li>`);
+  $('#skill').val('');
+});
+
+$('#save').click((e) => {
+  console.table(skills);
 });
